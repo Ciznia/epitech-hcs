0683a558b1ee658174df433a452c5e74b581286d
Ciznia <gabriel.hosquet@epitech.eu>
2025-01-08 20:03:37 +0100

    Release 1.0.0 WIP

--- README.md
+++ README.md
@@ -1,53 +1,30 @@
-# epitech-hcs README
+# epitech-cs README
 
-This is the README for your extension "epitech-hcs". After writing up a brief description, we recommend including the following sections.
+Extension to adapt the ecsls plugin from vim to vscode
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+Display in real time your coding style error
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+ecsls binary which can be install within your nixos configuration ecsls.packages.$[pkgs.system].ecsls
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+* `epitech-cs.enable`: Enable/disable this extension.
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Not working for now, it's in dev mode
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+Not working
 
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
+Initial release of nothing
 
 ## Following extension guidelines
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let clients: LanguageClient[] = [];
+
 export function activate(context: vscode.ExtensionContext) {
+  // Configure the EHCS Language Server (ehcsls)
+  const ehcslsServerOptions: ServerOptions = {
+    command: "ehcsls_run", // Command to start the EHCS Language Server
+    args: [],
+  };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "epitech-hcs" is now active!');
+  const ehcslsClientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "hs" }], // Target h files
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher("**/.git"),
+    },
+  };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('epitech-hcs.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Epitech hcs!');
-	});
+  const ehcslsClient = new LanguageClient(
+    "ehcsls",
+    "EHCS Language Server",
+    ehcslsServerOptions,
+    ehcslsClientOptions
+  );
 
-	context.subscriptions.push(disposable);
+  // Start both clients
+  ehcslsClient.start();
+
+  // Store clients in the array for later use
+  clients.push(ehcslsClient);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+  // Stop all clients during deactivation
+  return Promise.all(clients.map(client => client.stop())).then(() => undefined);
+}
